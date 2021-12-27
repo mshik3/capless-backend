190b52a1ea6491242a61313135f3b2c0cdedf6d8
Mustafa Shikora <mshik3@gmail.com>
2021-12-27 10:16:06 -0600

    Adding specific GET breakdown

--- capless_backend/capless_backend_stack.py
+++ capless_backend/capless_backend_stack.py
@@ -16,4 +16,9 @@ class CaplessBackendStack(Stack):
             code=_lambda.Code.from_asset('resources'),
         )
 
-        api = apigw.LambdaRestApi(self, "GetFeedEndpoint", handler=getFeedLambda)
+        api = apigw.LambdaRestApi(self, "GetFeedEndpoint", handler=getFeedLambda, proxy=False)
+        items = api.root.add_resource("feed")
+        items.add_method("GET") # Gets the entire feed /feed
+
+        item = items.add_resource("{feed_item}")
+        item.add_method("GET") # Gets a companies details from the feed /feed/{company_name}
