debb5ce2f53b6beb90e116cd3346af79f1b907e5
Mustafa Shikora <mshik3@gmail.com>
2021-12-27 11:21:02 -0600

    Adding new bucket and adding permissions to lambda for bucket

--- capless_backend/capless_backend_stack.py
+++ capless_backend/capless_backend_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     App,
     aws_apigateway as apigw,
     aws_lambda as _lambda,
+    aws_s3 as _s3,
     Stack
 )
 
@@ -9,12 +10,16 @@ class CaplessBackendStack(Stack):
 
     def __init__(self, scope: App, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        startup_bucket = _s3.Bucket(self, "capless-startup-data", bucket_name="capless-startup-data")
         
         getFeedLambda = _lambda.Function(self,'GetFeedLambda',
             handler='GetFeedLambda.lambda_handler',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('resources'),
         )
+
+        startup_bucket.grant_read(getFeedLambda)
 
         api = apigw.LambdaRestApi(self, "GetFeedEndpoint", handler=getFeedLambda, proxy=False)
         items = api.root.add_resource("feed")
