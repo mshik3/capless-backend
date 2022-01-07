88eba0bdddd3f8dc6cb3fb62d98e121949686230
brianrt <bthomp2000@gmail.com>
2022-01-06 19:57:01 -0600

    Allow any site to call our feed endpoint

--- resources/GetFeedLambda.py
+++ resources/GetFeedLambda.py
@@ -21,5 +21,10 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET'
+        },
         'body': file_content
     }
