1d5ccf67f21cb849933bcdd6e6adc29050ac19a5
Mustafa Shikora <mustafashikora@Mustafas-MBP.cable.rcn.com>
2022-02-27 21:38:28 -0600

    updating headers

--- resources/UserLambda.py
+++ resources/UserLambda.py
@@ -27,5 +27,10 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET'
+        },
         'body': "something"
     }
