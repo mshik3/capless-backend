8b4e36e3979e9db3d3de1fe9e6f0ff61e2125165
brianrt <bthomp2000@gmail.com>
2022-04-26 20:34:57 -0500

    Update StartupLambda to match InvestorLambda

--- resources/StartupLambda.py
+++ resources/StartupLambda.py
@@ -10,36 +10,37 @@ def lambda_handler(event, context):
 
     if method == "PUT":
 
-        # print("event: " + str(event))
+        print("event: " + str(event))
 
-        # str_body = event["body"]
+        str_body = event["body"]
 
-        # body = json.loads(str_body)
+        body = json.loads(str_body)
 
-        # if body is None:
-        #     body = ""
+        if body is None:
+            body = ""
 
-        # table = dynamodb.Table("CompanyInfo")
-        # response = table.put_item(
-        #     Item={
-        #         "company_email": body["company_email"],
-        #         "location": body["location"],
-        #         "zip_code": body["zip_code"],
-        #         "company_name": body["company_name"],
-        #         "linkedIn_link": body["linkedIn_link"],
-        #         "venture_experience": body["venture_experience"],
-        #         "investment_region": body["investment_region"],
-        #         "investment_check_size": body["investment_check_size"],
-        #         "referral": body["referral"],
-        #         "industries": body["industries"],
-        #         "investment_demographic": body["investment_demographic"],
-        #         "investment_description": body["investment_description"],
-        #         "admins": body["admins"],
-        #         "employees": body["employees"]
-        #     }
-        # )
+        table = dynamodb.Table("StartupInfo")
+        response = table.put_item(
+            Item={
+                "company_id": body["company_id"],
+                "company_email": body["company_email"],
+                "location": body.get("location"),
+                "zip_code": body.get("zip_code"),
+                "company_name": body.get("company_name"),
+                "linkedIn_link": body.get("linkedIn_link"),
+                "venture_experience": body.get("venture_experience"),
+                "investment_region": body.get("investment_region"),
+                "investment_check_size": body.get("investment_check_size"),
+                "referral": body.get("referral"),
+                "industries": body.get("industries"),
+                "investment_demographic": body.get("investment_demographic"),
+                "investment_description": body.get("investment_description"),
+                "admins": body.get("admins"),
+                "employees": [body.get("user_id")],
+            }
+        )
 
-        # print(response)
+        print(response)
 
         return {
             "statusCode": 200,
